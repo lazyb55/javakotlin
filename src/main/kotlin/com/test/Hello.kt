fe3bf43db95f92e49e887341a12dbb175b5839dd
楊鼎瀚 <yangdinghan@yangdinghande-MacBook-Air.local>
2019-03-03 15:02:26 +0800

    Initial commit

---
+++ src/main/kotlin/com/test/Hello.kt
@@ -0,0 +1,15 @@
+package com.test
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+//    Human().Hello()
+        val h = Human()
+        h.Hello()
+}
+
+
+class Human{
+    fun Hello(){
+        println("Hello Kotlin")
+    }
+}
